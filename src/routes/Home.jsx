185542bba41913402e2eabc4a121c7bfb48cf190
Aurélie <nn.aurelie@hotmail.com>
2021-10-25 11:41:38 +0200

    Setup redux store.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getPalettes } from '../reducers/paletteReducer';
 import Cards from '../components/palettes/Cards';
-import palettes from '../data/palettes';
 
-const Home = () => (
-  <div>
-    <Cards palettes={palettes} />
-  </div>
-);
+const Home = () => {
+  const palettes = useSelector(getPalettes);
+
+  return (
+    <div>
+      <Cards palettes={palettes} />
+    </div>
+  );
+};
 
 export default Home;
