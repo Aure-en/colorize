72a02268858fc923670bc32b8122ea51829b3e35
Aurélie <nn.aurelie@hotmail.com>
2021-10-28 14:55:16 +0200

    Setup store and component to display copy pop-ups

---
+++ src/reducers/copy.js
@@ -0,0 +1,27 @@
+import { ADD_COPY, REMOVE_COPY } from '../actions/copy';
+
+export const initialState = [];
+
+const settings = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case ADD_COPY:
+      return [
+        ...state,
+        {
+          x: action.x,
+          y: action.y,
+          id: action.id,
+        },
+      ];
+
+    case REMOVE_COPY:
+      return [...state].filter((copy) => copy.id !== action.id);
+
+    default:
+      return state;
+  }
+};
+
+export const getCopied = (state) => state.copy;
+
+export default settings;
