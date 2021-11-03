8ccb61068a342101342c9c0459ad91320ac96bdb
sullyvaaan <sullyvan.f@outlook.fr>
2021-11-03 14:05:53 +0100

    Pagination Update

--- src/components/Shared/Pagination.jsx
+++ src/components/Shared/Pagination.jsx
@@ -1,66 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
-function Pagination() {
+export default function BasicPagination() {
   return (
     <PaginationDropdown>
-      <BtnPrevious>
-        &#8249;
-      </BtnPrevious>
-      <Number href="#">
-        1
-      </Number>
-      <Number href="#">
-        2
-      </Number>
-      <Number href="#">
-        3
-      </Number>
-      <Number href="#">
-        4
-      </Number>
-      <Number href="#">
-        5
-      </Number>
-      <BtnNext>
-        &#8250;
-      </BtnNext>
+      <Stack spacing={2}>
+        <Pagination count={10} color="primary" />
+      </Stack>
     </PaginationDropdown>
   );
 }
 
 const PaginationDropdown = styled.div`
 display: flex;
-align-items: ;
 justify-content: center;
-
+margin-bottom: 1em;
 `;
-
-const Number = styled.a`
-display: flex;
-padding: 5px;
-text-decoration: none;
-display: inline-block;
-
-&:hover: {
-    background-color: #fff;
-    color: black;
-  }
-`;
-
-const BtnPrevious = styled.button`
-display: flex;
-text-decoration: none;
-color: #04AA6D;
-&:hover: {
-    background-color: #fff;
-    color: black;
-  }
-`;
-
-const BtnNext = styled.button`
-text-decoration: none;
-display: flex;
-color: #04AA6D;
-`;
-export default Pagination;
