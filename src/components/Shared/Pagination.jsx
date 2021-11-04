86bfa5b9c48abfc7962cba5b43dfe7c2fe14095a
sullyvaaan <sullyvan.f@outlook.fr>
2021-11-04 16:28:59 +0100

    Working on theme props for pagination mui

--- src/components/Shared/Pagination.jsx
+++ src/components/Shared/Pagination.jsx
@@ -2,19 +2,35 @@ import React from 'react';
 import styled from 'styled-components';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
+import { withStyles } from '@mui/styles';
+import { useTheme } from 'styled-components';
 
-export default function BasicPagination() {
+const theme = useTheme();
+const styles = {
+  root: {
+    color: theme.textPrimary,
+  },
+};
+
+export default function BasicPagination(props) {
+  const { classes } = props; 
   return (
     <PaginationDropdown>
       <Stack spacing={2}>
-        <Pagination count={10} color="primary" />
+          <Pagination className={classes.root} count={10} variant="outlined" color="primary" />
       </Stack>
     </PaginationDropdown>
   );
 }
+
+HigherOrderComponent.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 const PaginationDropdown = styled.div`
   display: flex;
   justify-content: center;
   color: ${(props) => props.theme.textPrimary};
 `;
+
+export withStyles(styles)(HigherOrderComponent);
