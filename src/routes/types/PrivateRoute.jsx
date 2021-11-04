5d1226a0bc7579edbabec8f9628488195c018501
Aurélie <nn.aurelie@hotmail.com>
2021-11-04 13:09:00 +0100

    Private and public routes

---
+++ src/routes/types/PrivateRoute.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getIsLoggedIn } from '../../selectors/user';
+
+// User can only access those routes if they are logged in.
+function PrivateRoute({ component: Component, render: Render, ...rest }) {
+  const isLoggedIn = useSelector(getIsLoggedIn);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => (isLoggedIn ? (
+        Render ? (
+          Render(props)
+        ) : Component ? (
+          <Component {...props} />
+        ) : null
+      ) : (
+        <Redirect to="/signin" />
+      ))}
+    />
+  );
+}
+
+export default PrivateRoute;
