a954053c2464fa5406c1c54118c7c581681ef951
Aurélie <nn.aurelie@hotmail.com>
2021-11-04 11:04:39 +0100

    Indicator displayed when a user has not created any palettes.

---
+++ src/components/Profile/NoPalettes.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const NoPalettes = () => (
+  <Wrapper>
+    <Content>
+      <Heading>There is nothing there.</Heading>
+      <Text>
+        <p>This user has no created any palettes yet.</p>
+      </Text>
+      {/* &#8592; = ← */}
+      <HomeLink to="/">&#8592; Take a step back</HomeLink>
+    </Content>
+  </Wrapper>
+);
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 25rem;
+  text-align: center;
+  padding: 2rem 5rem;
+`;
+
+const Heading = styled.div`
+  font-size: 1.25rem;
+  color: ${(props) => props.theme.primaryText};
+`;
+
+const HomeLink = styled(Link)`
+  color: ${(props) => props.theme.background};
+  background: ${(props) => props.theme.textPrimary};
+  padding: 0.75rem 1rem;
+  text-transform: uppercase;
+  font-size: 0.925rem;
+  border: none;
+  transition: background-color 0.2s ease-out;
+  justify-self: end;
+  margin-top: 1.5rem;
+
+  &:hover {
+    background: ${(props) => props.theme.primaryText};
+  }
+`;
+
+const Text = styled.div`
+  margin-top: 1rem;
+
+  & > p {
+    margin-bottom: 1rem;
+  }
+`;
+
+export default NoPalettes;
