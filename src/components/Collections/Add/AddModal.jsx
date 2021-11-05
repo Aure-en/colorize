fd2806641dc3413ed12d9981b81fc01bb8b8da86
Aurélie <nn.aurelie@hotmail.com>
2021-11-05 14:06:00 +0100

    Save Form

--- src/components/Collections/Add/AddModal.jsx
+++ src/components/Collections/Add/AddModal.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import useCreateCollection from '../../../hooks/collections/useCreateCollection';
+
 import Modal from '../../Shared/Modal/Modal';
 import BackButton from '../../Shared/Modal/BackButton';
 
