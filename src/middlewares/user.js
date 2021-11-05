5f438eebbabcb35cadbe75d2c8481ca0f3b798e0
sullyvaaan <sullyvan.f@outlook.fr>
2021-11-05 12:31:38 +0100

    Try Login

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -1,30 +1,35 @@
 import {
-  REQUEST_SIGNUP, successSignUp, REQUEST_LOGIN, successLogin,
+  REQUEST_SIGNUP, successSignUp, REQUEST_LOGIN, successLogin, requestLogin,
 } from '../actions/user';
 
 const userMiddleware = (store) => (next) => async (action) => {
-  console.log(action);
   switch (action.type) {
-    /*case REQUEST_SIGNUP: {
+    case REQUEST_SIGNUP: {
       const { dispatch } = store;
       const response = await fetch(
-        `${process.env.REACT_APP_SERVER}/user/`, { method: 'POST', body: JSON.stringify({username, email, password})},);
+        `${process.env.REACT_APP_SERVER}/user/`, { method: 'POST', body: JSON.stringify({ username: action.username, email: action.email, password: action.password }) },
+      );
 
       const json = await response.json();
-      dispatch(successSignUp(user, json));
-      break;
-    } */
+      dispatch(requestLogin(action.username,
+        json.id,
+        'token'));
+      break; }
 
     case REQUEST_LOGIN: {
-      console.log('text');
-      const responseAPI = {
-        username: 'username',
-        email: 'email',
-        jwt: 'monsupertoken',
-        id: 1,
-      };
+      console.log('text2');
+      const { dispatch } = store;
+      const response = await fetch(
+        'http://ec2-3-92-209-62.compute-1.amazonaws.com/projet-o-en-couleurs/public/api/login_check',
+        { method: 'POST', body: JSON.stringify({ email: action.identifier, password: action.password }) },
+      );
 
-      store.dispatch(successLogin(responseAPI.username, responseAPI.id, responseAPI.jwt));
+      const loginsuccess = await response.json();
+      console.log(loginsuccess);
+      dispatch(successLogin(loginsuccess.username,
+        loginsuccess.email,
+        loginsuccess.jwt,
+        loginsuccess.id));
       break;
     }
     default:
