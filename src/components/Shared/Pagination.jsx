84a4f68a63fabb317cf780fdc018484e2946d385
sullyvaaan <sullyvan.f@outlook.fr>
2021-11-05 09:13:52 +0100

    Fix Pagination

--- src/components/Shared/Pagination.jsx
+++ src/components/Shared/Pagination.jsx
@@ -2,35 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
-import { withStyles } from '@mui/styles';
-import { useTheme } from 'styled-components';
 
-const theme = useTheme();
-const styles = {
-  root: {
-    color: theme.textPrimary,
-  },
-};
-
-export default function BasicPagination(props) {
-  const { classes } = props; 
+export default function BasicPagination() {
   return (
     <PaginationDropdown>
       <Stack spacing={2}>
-          <Pagination className={classes.root} count={10} variant="outlined" color="primary" />
+        <Pagination count={10} variant="outlined" color="primary" />
       </Stack>
     </PaginationDropdown>
   );
 }
-
-HigherOrderComponent.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
 const PaginationDropdown = styled.div`
   display: flex;
   justify-content: center;
   color: ${(props) => props.theme.textPrimary};
 `;
-
-export withStyles(styles)(HigherOrderComponent);
