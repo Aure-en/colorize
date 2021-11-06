9172258dad4c79903e72fbbc40104c10f8a06476
Aurélie <nn.aurelie@hotmail.com>
2021-11-06 20:57:24 +0100

    Create collection.

--- src/middlewares/favorite.js
+++ src/middlewares/favorite.js
@@ -1,12 +1,17 @@
 import {
   REQUEST_SAVE_PALETTE,
   REQUEST_UNSAVE_PALETTE,
+  REQUEST_CREATE_COLLECTION,
+  REQUEST_UPDATE_COLLECTION,
+  REQUEST_DELETE_COLLECTION,
   savePalette,
   unsavePalette,
+  createCollection,
+  updateCollection,
+  deleteCollection,
 } from '../actions/favorite';
-import palettes from '../data/palettes';
 
-const favoriteMiddleware = (store) => (next) => (action) => {
+const favoriteMiddleware = (store) => (next) => async (action) => {
   switch (action.type) {
     case REQUEST_SAVE_PALETTE: {
       // API request
@@ -17,6 +22,29 @@ const favoriteMiddleware = (store) => (next) => (action) => {
     case REQUEST_UNSAVE_PALETTE: {
       // API request
       // Dispatch unsavePalette action.
+      break;
+    }
+
+    case REQUEST_CREATE_COLLECTION: {
+      const { user } = store.getState();
+
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER}/files/user/${user.id}`,
+        {
+          method: 'POST',
+          body: JSON.stringify({ name: action.name }),
+          headers: {
+            Authorization: `Bearer ${user.jwt}`,
+          },
+        },
+      );
+
+      const json = await response.json();
+
+      if (json.id) {
+        store.dispatch(createCollection({ name: action.name, collectionId: json.id }));
+      }
+
       break;
     }
 
