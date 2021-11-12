8505ff3eece04255feb41e3dc936b20f43d7ee80
Aurélie <nn.aurelie@hotmail.com>
2021-11-12 17:43:21 +0100

    First palette is now a featured palette selected by administrators and fetched from the API

--- src/middlewares/palette.js
+++ src/middlewares/palette.js
@@ -4,17 +4,29 @@ import {
   setMainPalette,
   setOriginalPalette,
 } from '../actions/palette';
-import palettes from '../data/palettes';
 
 const paletteMiddleware = (store) => (next) => async (action) => {
   switch (action.type) {
     case FETCH_FIRST_PALETTE: {
       const { dispatch } = store;
-      // API request to fetch the featured palette
+      // API request to fetch the featured palettes
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER}/palettes/features`,
+      );
+
+      const json = await response.json();
+      const palettes = json.list;
 
       // Choose a random palette from the featured ones.
       const paletteIndex = Math.floor(Math.random() * palettes.length);
-      const palette = palettes[paletteIndex];
+      const chosenPalette = palettes[paletteIndex];
+
+      // Get its owner
+      const paletteResponse = await fetch(
+        `${process.env.REACT_APP_SERVER}/palettes/${chosenPalette.id}/user`,
+      );
+
+      const palette = await paletteResponse.json();
 
       dispatch(setMainPalette(palette));
       dispatch(setOriginalPalette(palette));
