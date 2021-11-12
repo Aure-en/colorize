499da607ecc8d077739e33eddcff532001b7920b
Aurélie <nn.aurelie@hotmail.com>
2021-11-12 10:06:26 +0100

    Locked delete account option on sample account

--- src/components/Settings/SettingsPage/DeleteAccount/DeleteButton.jsx
+++ src/components/Settings/SettingsPage/DeleteAccount/DeleteButton.jsx
@@ -2,11 +2,30 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const DeleteButton = ({ openModal }) => (
-  <Button type="button" onClick={openModal}>
-    Delete your account
-  </Button>
-);
+import { useSelector } from 'react-redux';
+import { getUser } from '../../../../selectors/user';
+
+const DeleteButton = ({ openModal }) => {
+  const user = useSelector(getUser);
+
+  return (
+    <>
+      {user.id === Number(process.env.REACT_APP_SAMPLE_ID) ? (
+        <Button
+          type="button"
+          disabled
+          title="This option is not available on the sample account"
+        >
+          Delete your account
+        </Button>
+      ) : (
+        <Button type="button" onClick={openModal}>
+          Delete your account
+        </Button>
+      )}
+    </>
+  );
+};
 
 DeleteButton.propTypes = {
   openModal: PropTypes.func.isRequired,
@@ -15,6 +34,7 @@ DeleteButton.propTypes = {
 const Button = styled.button`
   color: ${(props) => props.theme.textSecondary};
   font-size: 0.925rem;
+  cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
 
   &:hover {
     color: ${(props) => props.theme.primaryText};
